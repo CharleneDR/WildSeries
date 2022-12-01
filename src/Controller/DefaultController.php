228e57c75614d2a400cfe50d6c35eeab3ff27334
CharleneDR <chdarugna.polytech@gmail.com>
2022-12-01 12:41:14 +0100

    quête 13

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,12 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $categories = $categoryRepository->findAll();
+        return $this->render('home/index.html.twig', ['categories' => $categories]);
     }
 } 
